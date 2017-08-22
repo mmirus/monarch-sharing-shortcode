66399b1544574bc9124aebc13cc2766726a996b1
Matthew Mirus <mmirus@afphq.org>
2017-08-22 12:43:10 -0400

    Initial release.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# monarch-sharing-shortcode
-Add a shortcode for adding inline Monarch sharing buttons.
+# Monarch Sharing Shortcode
+A WordPress plugin that provides a shortcode for adding inline Monarch sharing buttons.
+
+Credit to Shahar's comment <a href="https://journalxtra.com/wordpress/quicksnips/how-to-add-a-social-sharing-shortcode-to-monarch-from-elegant-themes/#comment-36511">here</a> for getting the ball rolling.
+
+Usage:
+
+*Sharing buttons with default URL (current page)*
+```
+[monarch_share]
+```
+
+*Sharing buttons with custom URL*
+```
+[monarch_share url="custom url"]
+```
---
+++ monarch-sharing-shortcode.php
@@ -0,0 +1,73 @@
+<?php
+/*
+Plugin Name: Monarch Sharing Shortcode
+Plugin URI: https://github.com/mmirus/monarch-sharing-shortcode
+Description: Provides a shortcode for adding inline Monarch sharing buttons.
+Author: Matt Mirus
+Author URI: https://github.com/mmirus
+Version: 1.0
+GitHub Plugin URI: https://github.com/mmirus/monarch-sharing-shortcode
+*/
+
+namespace MSS;
+
+// Add Shortcode
+function monarch_sharing_shortcode($atts) {
+	// Attributes
+	$atts = shortcode_atts(
+		[ 'url' => '', ],
+		$atts,
+		'monarch_share'
+	);
+  
+  return generate_inline_icons('et_social_inline_bottom', $atts['url']);
+}
+add_shortcode('monarch_share', 'monarch_sharing_shortcode');
+
+// copied from monarch.php and edited to allow manually specifying URL
+// edits: function arguments; set $monarch to global Monarch instance; replace $this with $monarch; and pass URL to get_icons_list
+function generate_inline_icons($class = 'et_social_inline_bottom', $url = '') {
+  $monarch = $GLOBALS['et_monarch'];
+  $monarch_options = $monarch->monarch_options;
+
+  if ( isset( $monarch_options[ 'general_main_reset_postdata' ] ) && true == $monarch_options[ 'general_main_reset_postdata' ] ) {
+    wp_reset_postdata();
+  }
+
+  $display_all_button = isset( $monarch_options[ 'sharing_inline_display_all' ] ) ? $monarch_options[ 'sharing_inline_display_all' ] : false;
+
+  $inline_content = sprintf(
+    '<div class="et_social_inline%10$s %12$s">
+      <div class="et_social_networks et_social_%2$s et_social_%3$s et_social_%4$s et_social_%5$s et_social_no_animation%6$s%7$s%9$s%11$s%13$s">
+        %8$s
+        %1$s
+      </div>
+    </div>',
+    $monarch->get_icons_list( 'inline', '', false, $display_all_button, false, '', $url ),
+    'auto' == $monarch_options[ 'sharing_inline_col_number' ]
+      ? 'autowidth'
+      : esc_attr( $monarch_options[ 'sharing_inline_col_number' ] . 'col' ),
+    esc_attr( $monarch_options[ 'sharing_inline_icon_style' ] ),
+    esc_attr( $monarch_options[ 'sharing_inline_icon_shape' ] ),
+    esc_attr( $monarch_options[ 'sharing_inline_icons_alignment' ] ), //#5
+    true == $monarch_options[ 'sharing_inline_counts' ] ? ' et_social_withcounts' : '',
+    true == $monarch_options[ 'sharing_inline_total' ] ? ' et_social_withtotalcount' : '',
+    true == $monarch_options[ 'sharing_inline_total' ]
+      ? sprintf(
+        '<div class="et_social_totalcount">
+          <span class="et_social_totalcount_count et_social_total_share" data-post_id="%2$s"></span>
+          <span class="et_social_totalcount_label">%1$s</span>
+        </div>',
+        esc_html__( 'Shares', 'Monarch' ),
+        esc_attr( get_the_ID() )
+      )
+      : '',
+    true == $monarch_options[ 'sharing_inline_spacing' ] ? ' et_social_nospace' : '',
+    true == $monarch_options[ 'sharing_inline_mobile' ] ? ' et_social_mobile_off' : ' et_social_mobile_on', //#10
+    true == $monarch_options[ 'sharing_inline_network_names' ] ? ' et_social_withnetworknames' : '',
+    esc_attr( $class ),
+    esc_attr( sprintf( ' et_social_outer_%1$s', $monarch_options[ 'sharing_inline_outer_color' ] ) ) //#13
+  );
+
+  return $inline_content;
+}
